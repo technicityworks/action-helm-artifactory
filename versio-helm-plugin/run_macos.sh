184a0bb04053c564d6105df55aff17b50d05cedc
André Nogueira <aanogueira@protonmail.com>
2022-09-16 20:27:18 +0100

    Fixed validations

--- versio-helm-plugin/run_macos.sh
+++ versio-helm-plugin/run_macos.sh
@@ -46,14 +46,14 @@ COMMAND=${PASSTHRU[0]}
 
 if [ "$COMMAND" == "increment" ]; then
   CHART_PATH=${PASSTHRU[1]}
-  if [ "$CHART_PATH" == "" ]; then
+  if [ ""$CHART_PATH"" == "" ]; then
     echo "Chart path was not provided"
     exit 1
   fi
-  CURRENT=$(cat $CHART_PATH/Chart.yaml | grep '^version:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
-  INCREMENTED=$(echo $CURRENT | awk -F. -v OFS=. '{$NF += 1 ; print}')
-  gsed -i "s\version: $CURRENT\version: $INCREMENTED\g" $CHART_PATH/Chart.yaml
-  CHART_NAME=$(cat $CHART_PATH/Chart.yaml | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
+  CURRENT=$(cat "$CHART_PATH/Chart.yaml" | grep '^version:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
+  INCREMENTED=$(echo "$CURRENT" | awk -F. -v OFS=. '{$NF += 1 ; print}')
+  gsed -i "s\version: $CURRENT\version: $INCREMENTED\g" "$CHART_PATH/Chart.yaml"
+  CHART_NAME=$(cat "$CHART_PATH/Chart.yaml" | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
   echo "Chart '$CHART_NAME' update from version '$CURRENT' to version '$INCREMENTED'"
   exit 0
 elif [ "$COMMAND" == "validate" ]; then
@@ -63,18 +63,18 @@ elif [ "$COMMAND" == "validate" ]; then
     exit 1
   fi
   CHART_REPO=${PASSTHRU[2]}
-  CURRENT=$(cat $CHART_PATH/Chart.yaml | grep '^version:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
-  CHART_NAME=$(cat $CHART_PATH/Chart.yaml | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
-  PUBLISHED=$(helm search repo $CHART_REPO/$CHART_NAME -o json | cut -d '"' -f 8)
+  CURRENT=$(cat "$CHART_PATH/Chart.yaml" | grep '^version:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
+  CHART_NAME=$(cat "$CHART_PATH/Chart.yaml" | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
+  PUBLISHED=$(helm search repo "$CHART_REPO"/"$CHART_NAME" -o json | cut -d '"' -f 8)
 
   if [ "$CHART_REPO" == "" ]; then
     echo "Chart repo not provided, will search for '$CHART_NAME' on all repos"
-    FOUND=$(helm search repo $CHART_NAME -o json | cut -d '/' -f 1 | cut -d '"' -f4)
+    FOUND=$(helm search repo "$CHART_NAME" -o json | cut -d '/' -f 1 | cut -d '"' -f4)
     if [ "$PUBLISHED" == "[]" ]; then
       echo "Chart '$CHART_NAME' was not found on any repo"
       exit 3
     else
-      if [ "$CURRENT" == $PUBLISHED ]; then
+      if [ "$CURRENT" == "$PUBLISHED" ]; then
         echo "Chart '$CHART_NAME' was found in '$FOUND' repo"
         if [ "$IGNORE" == true ]; then
           echo "Chart '$CHART_NAME' version '$CURRENT' hasn't been updated, ignoring..."
@@ -92,7 +92,7 @@ elif [ "$COMMAND" == "validate" ]; then
       echo "Chart '$CHART_NAME' was not found in '$CHART_REPO' repo"
       exit 3
     else
-      if [ "$CURRENT" == $PUBLISHED ]; then
+      if [ "$CURRENT" == "$PUBLISHED" ]; then
         if [ "$IGNORE" == true ]; then
           echo "Chart '$CHART_NAME' version '$CURRENT' hasn't been updated, ignoring..."
           exit 0
@@ -108,16 +108,16 @@ elif [ "$COMMAND" == "validate" ]; then
   exit 2
 elif [ "$COMMAND" == "lookup" ]; then
   CHART_PATH=${PASSTHRU[1]}
-  if [ "$CHART_PATH" == "" ]; then
+  if [ ""$CHART_PATH"" == "" ]; then
     echo "Chart path was not provided"
     exit 1
   fi
   CHART_REPO=${PASSTHRU[2]}
-  CHART_NAME=$(cat $CHART_PATH/Chart.yaml | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
-  PUBLISHED=$(helm search repo $CHART_REPO/$CHART_NAME -o json | cut -d '"' -f 8)
+  CHART_NAME=$(cat "$CHART_PATH/Chart.yaml" | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
+  PUBLISHED=$(helm search repo "$CHART_REPO"/"$CHART_NAME" -o json | cut -d '"' -f 8)
   if [ "$CHART_REPO" == "" ]; then
     echo "Chart repo not provided, will search for '$CHART_NAME' on all repos"
-    FOUND=$(helm search repo $CHART_NAME -o json | cut -d '/' -f 1 | cut -d '"' -f4)
+    FOUND=$(helm search repo "$CHART_NAME" -o json | cut -d '/' -f 1 | cut -d '"' -f4)
     if [ "$PUBLISHED" == "[]" ]; then
       echo "Chart '$CHART_NAME' was not found any repo"
       exit 3
@@ -136,12 +136,12 @@ elif [ "$COMMAND" == "lookup" ]; then
   exit 0
 elif [ "$COMMAND" == "print" ]; then
   CHART_PATH=${PASSTHRU[1]}
-  if [ "$CHART_PATH" == "" ]; then
+  if [ ""$CHART_PATH"" == "" ]; then
     echo "Chart path was not provided"
     exit 1
   fi
-  CURRENT=$(cat $CHART_PATH/Chart.yaml | grep '^version:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
-  CHART_NAME=$(cat $CHART_PATH/Chart.yaml | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
+  CURRENT=$(cat "$CHART_PATH/Chart.yaml" | grep '^version:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
+  CHART_NAME=$(cat "$CHART_PATH/Chart.yaml" | grep '^name:' | cut -d':' -f 2 | tr -d '"' | tr -d ' ')
   echo "Chart '$CHART_NAME' current version is '$CURRENT'"
   exit 0
 else
